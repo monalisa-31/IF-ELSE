10ccb2d33f3d7d15e5a6facb115f1b281d91744c
Monalisa Mishra <97949602+monalisa-31@users.noreply.github.com>
2022-12-16 15:50:02 +0530

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,13 @@
+def fun ():
+    i= -1
+    b = []
+    x = [5,3,2,4,6]
+    while i <= (-len(x)):
+        b.append(x[i])
+        i-=1
+    print(b)
+
+fun()
+# a= " How do you"
+# a.lstrip()
+        
